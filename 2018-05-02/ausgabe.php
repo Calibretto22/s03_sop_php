d3dc51a75501b562c24b6275bc327a9cef71a528
Calibretto <github.com@sentinelx.de>
2018-05-02 20:18:11 +0200

    üble variablen gabe

---
+++ 2018-05-02/ausgabe.php
@@ -0,0 +1,10 @@
+<?php
+#kommentar: what a ride
+$anrede['pvname'] = $_POST['vname'];
+$anrede['pnname'] = $_POST['nname'];
+
+#echo "hallo welt - die zweite - mit Ümläut-Töst </br>";
+echo "hallo ".$anrede['pvname']." ".$anrede['pnname'];
+
+
+?>
