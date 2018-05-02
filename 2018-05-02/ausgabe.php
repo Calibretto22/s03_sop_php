497488b2531ca16f9c8c4db5d6ae734cd9f3da5b
Calibretto <github.com@sentinelx.de>
2018-05-02 20:43:26 +0200

    prepare output

--- 2018-05-02/ausgabe.php
+++ 2018-05-02/ausgabe.php
@@ -8,3 +8,20 @@ echo "hallo ".$anrede['pvname']." ".$anrede['pnname'];
 
 
 ?>
+
+<!--
+<html>
+<head><title>Begruessung</title></head>
+<body>
+<form action="ausgabe.php" method=post>
+
+	<input name="vname" type=text size=40 maxlength=80 value="Bobby">
+	</br>
+	<input name="nname" type=text size=40 maxlength=80 value="Tables">
+	</br>
+	<input name="btnsend" type="submit" value="Abschicken">
+	<input name="btnreset" type="reset" value="Zuruecksetzen">
+</form>
+</body>
+</html>
+-->
